a74f0551b203acf1f19ca052e0310910a5c04ba3
Mohammed Umair <umairinter75@gmail.com>
2026-06-18 21:48:33 +0530

    Compare_int program from Week 4

---
+++ compare_int.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 50;
+    int j = 50;
+
+    if (i == j)
+    {
+        printf("Same\n");
+    }
+
+    else
+    {
+        printf("Different\n");
+    }
+}
+// 
